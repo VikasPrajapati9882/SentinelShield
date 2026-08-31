d8ca620d3f888f27b1f1cd5698c81fa32376400d
VikasPrajapati9882 <vikasprajapati9882@gmail.com>
2026-08-31 13:45:38 +0530

    Modify socketio.run to use dynamic port
    Updated socketio.run to include port from environment variable.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from database import get_statistics
 from functools import wraps
 from database import update_alert_info
 import time
+import os
 from flask import session
 from flask_socketio import SocketIO
 from database import get_attack_statistics
@@ -254,7 +255,7 @@ def dashboard():
 
 if __name__ == "__main__":
     socketio.run(
-        host='0.0.0.0',
         app,
-        debug=True
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
     )
